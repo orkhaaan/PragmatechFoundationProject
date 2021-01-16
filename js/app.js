8f0d4c7ac01bc6d08b8a68a6a2dbec5dba75013a
orkhaaan <76060157+orkhaaan@users.noreply.github.com>
2021-01-17 00:43:40 +0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,35 @@
+// let a=1
+// function capet() {
+//     console.log(a)
+//     a++
+//     if (a<100){
+//         capet()
+//     }
+//     else{
+
+//     }
+    
+// }
+// capet()
+// let a=1
+// while(a<400){
+//     console.log(a);
+//     a++;
+// }
+// for(let a=1; a<400;a++){
+//    if(a%60==0 )
+//    console.log(a)
+// }
+// let a=4;
+// function foo() {
+//     let a=5;
+//     console.log(a)
+    
+// }
+// console.log(a)
+// foo()
+
+let squad04=['edfasf','sadasd','asdasd']
+for(i=0;i<4;i++){
+    console.log(squad04[i])
+}
