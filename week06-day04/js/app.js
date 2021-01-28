8f30c37ca2883e637e2af457ff41a023bf9ef43a
Orkhaan Babazada <baba.orkhaan@mail.ru>
2021-01-28 15:02:05 +0400

    week06-day04

---
+++ week06-day04/js/app.js
@@ -0,0 +1,18 @@
+// $('.card').click(function(){
+//     $(this).hide(3000,function(){
+//         alert('div hidden')
+//     })
+// })
+
+function getFormData(){
+    let ad=document.querySelector('#ad')
+    let soyad=document.querySelector('#Soyad')
+    let seher=document.querySelector('#Seher')
+
+
+    console.log(ad.value)
+    console.log(soyad.value)
+    console.log(seher.value)
+
+
+}
