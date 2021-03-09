77e947d4ee78877f8d5c2697a577c0031c1e2cf2
Orkhaan Babazada <baba.orkhaan@mail.ru>
2021-03-09 23:06:00 +0400

    improve flask

--- js1/animation.js
+++
@@ -1,9 +0,0 @@
-let btn=document.querySelector('.btn')
-let fun=document.querySelector('.fun')
-
-leftPos=15
-btn.addEventListener('click',function(){
-    fun.style.left=leftPos+'px';
-    leftPos+=20
-
-})
